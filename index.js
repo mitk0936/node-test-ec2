f07df5ed018aebf0e7ac35e1cd58cc2807333170
Dimitar Mihaylov <dimitar.mihaylov@siteground.com>
2020-04-01 12:30:03 +0300

    Update port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+console.log(...process.argv);
 
-app.listen(port, '0.0.0.0', () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, '0.0.0.0', () => console.log(`Example app listening on port ${port}!`));
